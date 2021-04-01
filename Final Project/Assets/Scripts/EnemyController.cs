75a469248ba6e7d79fe4976b50a1bfa64214f32d
sherbracewell <70412782+sherbracewell@users.noreply.github.com>
2021-04-01 19:18:06 -0400

    Enemy Movement and Scene Management

---
+++ Final Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+     public GameObject[] points;
+     private int currentPath = 0;
+     private float lastPoint;
+     public float speed = 1.0f;
+
+    void Start()
+    {
+         lastPoint = Time.time;
+    }
+
+    void Update()
+    {   
+         Vector3 startPosition = points [currentPath].transform.position;
+         Vector3 endPosition = points [currentPath + 1].transform.position;
+
+         float pathLength = Vector3.Distance (startPosition, endPosition);
+         float totalTime = pathLength / speed;
+         float currentTime = Time.time - lastPoint;
+
+         gameObject.transform.position = Vector2.Lerp (startPosition, endPosition, currentTime / totalTime);
+
+            if (gameObject.transform.position.Equals(endPosition)) 
+                {
+                    if (currentPath < points.Length - 2)
+                        {
+                            currentPath++;
+                            lastPoint = Time.time;
+                        }
+
+                    else
+                        {
+                            Destroy(gameObject);
+                        }
+                }
+    }
+
+
+
+}
---
+++ Final Project/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    public GameObject[] points;
+    public GameObject EnemyPrefab;
+
+
+    void Start()
+    {
+        Instantiate(EnemyPrefab).GetComponent<EnemyController>().points = points;
+    }
+}
