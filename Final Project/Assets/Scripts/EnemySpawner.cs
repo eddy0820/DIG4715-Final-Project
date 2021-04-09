f43a7de72ee4203bc23585ade71895344532db22
Eddy Elguezabal <eddye0820@gmail.com>
2021-04-09 15:30:31 -0400

    Enemy Spawning

---
+++ Final Project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    public int spawnCount;
+
+    public float spawnDelay;
+    
+    public float spawnInterval;
+
+    private int numSpawned;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", spawnDelay, spawnInterval);
+    }
+
+    void Update()
+    {
+        if(numSpawned == spawnCount)
+        {
+            CancelInvoke();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        numSpawned++;
+        Instantiate(Enemy);
+    }
+
+}
