d810a4c6309ed06be0e89bf6540649bda4c47b46
Krzymnjoe <Krzymnjoe@gmail.com>
2021-04-09 22:37:56 -0400

    update

--- Final Project/Assets/Scripts/EnemyController.cs
+++ Final Project/Assets/Scripts/EnemyController.cs
@@ -2,77 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
-{
-     public GameObject[] points;
-     private int currentPath = 0;
-     private float lastPoint;
-     public float speed = 1.0f;
-     private int enemiesSpawned = 0;
+public class EnemyController : MonoBehaviour {
+    public GameObject[] points;
+    private int currentPath = 0;
+    private float lastPoint;
+    public float speed = 1.0f;
+    private int enemiesSpawned = 0;
 
-
-    private void Awake()
-    {
-        Enemies.enemies.Add(gameObject);
+    private void Awake () {
+        Enemies.enemies.Add (gameObject);
     }
 
-    void Start()
-    {
-         lastPoint = Time.time;
+    void Start () {
+        lastPoint = Time.time;
     }
 
-    void Update()
-    {   
-         Vector3 startPosition = points [currentPath].transform.position;
-         Vector3 endPosition = points [currentPath + 1].transform.position;
+    void Update () {
+        Vector3 startPosition = points[currentPath].transform.position;
+        Vector3 endPosition = points[currentPath + 1].transform.position;
 
-         float pathLength = Vector3.Distance (startPosition, endPosition);
-         float totalTime = pathLength / speed;
-         float currentTime = Time.time - lastPoint;
+        float pathLength = Vector3.Distance (startPosition, endPosition);
+        float totalTime = pathLength / speed;
+        float currentTime = Time.time - lastPoint;
 
-         gameObject.transform.position = Vector2.Lerp (startPosition, endPosition, currentTime / totalTime);
+        gameObject.transform.position = Vector2.Lerp (startPosition, endPosition, currentTime / totalTime);
 
-            if (gameObject.transform.position.Equals(endPosition)) 
-                {
-                    if (currentPath < points.Length - 2)
-                        {
-                            currentPath++;
-                            lastPoint = Time.time;
-                        }
-
-                    else
-                        {
-                            Destroy(gameObject);
-                        }
-                }
-    }
-
-    void Die()
-    {
-        Enemies.enemies.Remove(gameObject);
-        Destroy(transform.gameObject);
-        
-    }
-
-        void OnCollisionEnter2D(Collision2D other)
-    {
-        StationHealth enemy = other.gameObject.GetComponent<StationHealth>();
-
-        if (enemy != null)
-        {
-            enemy.ChangeHealth(-1);
-            Debug.Log("You lost health!");
-        }
-        
-        if (other.gameObject.CompareTag("Bullet"))
-        {
-            Die();
-            Destroy(this.gameObject);
+        if (gameObject.transform.position.Equals (endPosition)) {
+            if (currentPath < points.Length - 2) {
+                currentPath++;
+                lastPoint = Time.time;
+            } else {
+                Destroy (gameObject);
+            }
         }
     }
 
-    private void stopSpawning()
-    {
+    void Die () {
+        Enemies.enemies.Remove (gameObject);
+        Destroy (transform.gameObject);
+
+    }
+
+    void OnCollisionEnter2D (Collision2D other) {
+        StationHealth enemy = other.gameObject.GetComponent<StationHealth> ();
+
+        if (enemy != null) {
+            enemy.ChangeHealth (-1);
+            Debug.Log ("You lost health!");
+        }
+
+        if (other.gameObject.CompareTag ("Bullet")) {
+            Die ();
+            Destroy (this.gameObject);
+        }
+    }
+
+    private void stopSpawning () {
         enemiesSpawned--;
     }
 }
