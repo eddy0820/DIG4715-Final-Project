2be079d964f97d622b8e3b11abe7f46ab2fb1b0e
Krzymnjoe <Krzymnjoe@gmail.com>
2021-04-23 16:18:42 -0400

    update

--- Final Project/Assets/Scripts/SceneManger.cs
+++ Final Project/Assets/Scripts/SceneManger.cs
@@ -4,37 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SceneManger : MonoBehaviour
-{
+public class SceneManger : MonoBehaviour {
     public bool gameOver = false;
     public bool gameWin = false;
 
+    void Start () {
+        QuitGame ();
 
-    void Start()
-    {
-        
     }
-       public void SwitchScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName); 
-        
-        if (gameWin == true)
-            {
-                SceneManager.LoadScene("Win");
-            }
-        
-        if (gameOver == true)
-            {
-                SceneManager.LoadScene("Lose");
-            }
+    public void SwitchScene (string sceneName) {
+        SceneManager.LoadScene (sceneName);
+
+        if (gameWin == true) {
+            SceneManager.LoadScene ("Win");
+        }
+
+        if (gameOver == true) {
+            SceneManager.LoadScene ("Lose");
+        }
 
     }
 
-       public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Game is quitting");
+    public void QuitGame () {
+        if (Input.GetKey (KeyCode.Escape)) {
+            //Application.Quit ();
+            Debug.Log ("Game is quitting");
+        }
+
     }
 
-   
 }
